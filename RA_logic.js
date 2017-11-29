3526d7fe6efddc296d0b18cb264221a1bfd5357b
Cameron <cameron.grams.2@gmail.com>
2017-11-28 20:40:22 -0500

    with new key

--- RA_logic.js
+++ RA_logic.js
@@ -10,7 +10,7 @@ function returnSite(){
 function initMap(){
   var garageLocation = { lat: 34.724221, lng: -76.7370959};
   var mapProp = {
-    center: garageLocation, //need the exact location
+    center: garageLocation,
     zoom: 13,
     mapTypeId: 'roadmap'
   }
@@ -23,5 +23,4 @@ function initMap(){
 }
  
 
-// https://www.google.com/maps/place/Real+Automotive/@34.724221,-76.7370959,15z/data=!4m5!3m4!1s0x0:0x8782a82ac57a6d44!8m2!3d34.724221!4d-76.7370959
 
